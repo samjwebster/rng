601da789523e5d9c9301ff78128beb676d2fd633
samjwebster <samjwebster01@gmail.com>
2025-03-05 18:17:24 -0500

    added progress saving and timer

--- script.js
+++ script.js
@@ -1,7 +1,52 @@
-let numberDisplay = document.getElementById("random_number");
-let knownNumbers = new Set();
-
+let knownNumbers, timer;
 let max_number = 40000000;
+
+onload = function() {
+    loadProgress();
+    update_progress_bar();
+    updateTimerDisplay();
+    startTimer();
+}
+
+function saveProgress() {
+    let gameData = {
+        knownNumbers: Array.from(knownNumbers), // Convert the set to an array
+        timer: timer,
+        lastSave: Date.now()
+    };
+    localStorage.setItem("gameData", JSON.stringify(gameData));
+}
+
+function loadProgress() {
+    let savedData = localStorage.getItem("gameData");
+    if (savedData) {
+        let gameData = JSON.parse(savedData);
+        knownNumbers = new Set(gameData.knownNumbers);
+        timer = gameData.timer;
+        update_progress_bar();
+    } else {
+        console.log("no saved data, starting fresh");
+        knownNumbers = new Set();
+        timer = 0;
+    }
+}
+
+// Timer functions
+function updateTimerDisplay() {
+    let date = new Date(null);
+    date.setSeconds(timer);
+    let timerDisplay = document.getElementById("timer");
+    timerDisplay.innerHTML = date.toISOString().slice(11, 19);
+}
+
+function startTimer() {
+    // Every second, save the progress and update the timer visual
+    timerFunc = setInterval(function() {
+        timer += 1;
+        saveProgress();
+        updateTimerDisplay();
+    }, 1000);
+}
 
 function gen_random_number() {
     // Generate a random number between 1 and 40 million
